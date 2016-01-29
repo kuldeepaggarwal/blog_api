54f9473888ab343f6ac1e56344be007b6c530365
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2016-01-30 00:13:57 +0530

    we don't need seed data in `test` env. :stuck_out_tongue:

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,5 +1,6 @@
+require_relative './admin_seeder'
 require_relative './development/blogger_seeder'
 require_relative './development/blog_seeder'
 require_relative './development/comment_seeder'
 
-[BloggerSeeder, BlogSeeder, CommentSeeder].each(&:seed!)
+[AdminSeeder, BloggerSeeder, BlogSeeder, CommentSeeder].each(&:seed!)
